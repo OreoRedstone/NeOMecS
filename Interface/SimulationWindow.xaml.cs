cdf0ad0a043ae1d5dc58c1c1367332b5a088d864
OreoRedstone <isaac.the.owen@gmail.com>
2023-11-26 12:05:33 +0000

    Fixed alignment issues in the body sidebar.

--- Interface/SimulationWindow.xaml.cs
+++ Interface/SimulationWindow.xaml.cs
@@ -182,7 +182,7 @@ public partial class SimulationWindow : Window
                 HorizontalAlignment = HorizontalAlignment.Stretch,
                 Tag = body.guid,
                 Style = (Style)FindResource("LeftPanelBodyButton"),
-                Margin = new Thickness(body.GetParentNestingCount(simulation.universe) + 1 * 10, 1, 1, 1),
+                Margin = new Thickness((body.GetParentNestingCount(simulation.universe) + 1) * 10, 1, 1, 1),
             };
             bodyEntry.Click += new RoutedEventHandler(LeftPanelBodyButtonCall);
             BodySidebarGrid.Children.Add(bodyEntry);
