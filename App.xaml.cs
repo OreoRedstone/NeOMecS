31ec983ad063e03d8e9e6c4be26db6746454fc29
OreoRedstone <isaac.the.owen@gmail.com>
2023-12-03 11:41:31 +0000

    Implemented "Open With" functionality.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using NeOMecS.Interface;
+using NeOMecS.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +16,26 @@ namespace NeOMecS
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            string[] args = e.Args;
+            if(args.Length >= 1)
+            {
+                foreach (var arg in args)
+                {
+                    if(File.Exists(arg))
+                    {
+                        var window = new SimulationWindow(SaveLoadSystem.DecodeSimulation(SaveLoadSystem.Load(arg)), arg);
+                        window.Show();
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                var window = new StartupWindow();
+                window.Show();
+            }
+        }
     }
 }
