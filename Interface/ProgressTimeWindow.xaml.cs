300dffcce6079fe59733646a4d4f7451c805d769
OreoRedstone <isaac.the.owen@gmail.com>
2023-12-28 14:22:50 +0000

    Got fixed time progressing working.

--- Interface/ProgressTimeWindow.xaml.cs
+++ Interface/ProgressTimeWindow.xaml.cs
@@ -30,6 +30,8 @@ namespace NeOMecS.Interface
         double frequency = 0;
         double timePeriod = 0;
 
+        BackgroundWorker worker;
+
         public ProgressTimeWindow(SimState simulation)
         {
             InitializeComponent();
@@ -56,50 +58,66 @@ namespace NeOMecS.Interface
                 MessageBox.Show("Error while parsing the time period.");
                 return;
             }
-            BackgroundWorker worker = new();
+
+            worker = new BackgroundWorker();
             worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
             worker.DoWork += worker_DoWork;
             worker.ProgressChanged += worker_ProgressChanged;
             worker.RunWorkerCompleted += worker_RunWorkerCompleted;
             worker.RunWorkerAsync();
+            CurrentItemCount.Text = "Working...";
         }
 
         private void worker_RunWorkerCompleted(object? sender, RunWorkerCompletedEventArgs e)
         {
+            worker.Dispose();
+
+            if (e.Cancelled)
+            {
+                CurrentItemCount.Text = "Cancelled.";
+                return;
+            }
+
             var simWindow = (SimulationWindow)Application.Current.MainWindow;
             simWindow.simulation = new SimState(simulation);
             simWindow.UpdateBodySidebar();
             CurrentItemCount.Text = "Finished.";
+            ProgressBar.Value = 100;
             //Close();
         }
 
         private void worker_ProgressChanged(object? sender, ProgressChangedEventArgs e)
         {
             ProgressBar.Value = e.ProgressPercentage;
-            //CurrentItemCount.Text = e.UserState.ToString();
         }
 
         private void worker_DoWork(object? sender, DoWorkEventArgs e)
         {
+            Stopwatch sw = Stopwatch.StartNew();
             var localSim = new SimState(simulation);
             var localFrequency = frequency;
-            var worker = sender as BackgroundWorker;
             double length = frequency * timePeriod;
-            CurrentItemCount.Text = "Working...";
             for (int i = 0; i < length; i++)
             {
-                localSim = SimulationPhysics.SimulateStep(localSim, 1 / localFrequency);
-                //Thread.Sleep(1);
-                //double progress = i / length * 100;
+                if (worker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    break;
+                }
 
-                //worker.ReportProgress(Convert.ToInt32(progress));
+                localSim = SimulationPhysics.SimulateStep(localSim, 1 / localFrequency);
+                double progress = i / length * 100;
+
+                if(sw.ElapsedTicks % 1000 == 0) worker.ReportProgress(Convert.ToInt32(progress));
             }
-            simulation = new SimState(localSim);
+            if(!worker.CancellationPending) simulation = new SimState(localSim);
+            sw.Stop();
         }
 
         private void CancelButton_Click(object sender, RoutedEventArgs e)
         {
-            Close();
+            worker.CancelAsync();
         }
     }
 }
