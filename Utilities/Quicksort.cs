3fb90513b4dea40a90c917a10a3337009578eae8
OreoRedstone <58889498+OreoRedstone@users.noreply.github.com>
2024-01-05 16:15:10 +0000

    Update Quicksort.cs

--- Utilities/Quicksort.cs
+++ Utilities/Quicksort.cs
@@ -1,1 +1,16 @@
+using NeOMecS.Physics;
 
+namespace NeOMecS.Utilities;
+
+public static class Quicksort
+{
+  public static List<Body> SortBodiesByNesting(List<Body> list, int left, int right)
+  {
+    
+  }
+
+  private static List<Body> Sort(List<Body> list, int left, int right)
+  {
+
+  }
+}
